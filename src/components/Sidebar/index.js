0981be49bc00dba56dc9e8890acf5e313d6cca40
zardron <zardron.pesquera1993@gmail.com>
2023-07-08 11:31:23 +0400

    adding minor changes

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,5 @@
 import "./index.scss";
 import LogoS from "../../assets/images/logo-z.png";
-import codechefLogo from "../../assets/images/codechef.png";
 import LogoSubtitle from "../../assets/images/zardron_logo_white.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
